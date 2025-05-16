d6aa5b05ce1e90bfcf6aad07d650d793289a6eb3
Arian <ariankhadem4@gmail.com>
2025-05-17 00:19:20 +0700

    cors only from frontend api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_API,
+  });
   await app.listen(process.env.PORT ?? 4001);
 }
 bootstrap();
